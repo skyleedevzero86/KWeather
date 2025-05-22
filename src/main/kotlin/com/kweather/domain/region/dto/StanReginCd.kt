66e3a794ddc6aa3c203ce781392dd09dca089286
이경용 <sleekydz86@icloud.com>
2025-05-23 03:20:07 +0900

    feat:불필요한 코드정리

--- src/main/kotlin/com/kweather/domain/region/dto/StanReginCd.kt
+++
@@ -1,8 +0,0 @@
-package com.kweather.domain.region.dto
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class StanReginCd(
-    val row: List<RegionDto>?
-)
