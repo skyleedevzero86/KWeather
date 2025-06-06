f1d19c183bf4ef1bf1f943683c31b9822df394f5
이경용 <sleekydz86@icloud.com>
2025-06-06 23:58:19 +0900

    feat: 온도별 이벤트 추가

---
+++ src/test/kotlin/com/kweather/domain/weather/controller/AirStagnationIndexChartControllerTest.kt
@@ -0,0 +1,31 @@
+package com.kweather.domain.weather.controller
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class AirStagnationIndexChartControllerTest {
+    @BeforeEach
+    fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+    @AfterEach
+    fun tearDown() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun getAirStagnationIndexService() {
+    }
+
+    @Test
+    fun setAirStagnationIndexService() {
+    }
+
+    @Test
+    fun getAirStagnationChartData() {
+    }
+
+}
